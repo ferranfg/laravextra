e7fb18543ea87514d13a1625a635d2fa75a9aefa
Ferran Figueredo <ferranfg@gmail.com>
2016-01-07 14:07:37 +0100

    Service Provider

---
+++ src/Facades/Form.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ferranfg\Laravextra\Form;
+
+use Illuminate\Form\FormFacade;
+
+class Form extends FormFacade
+{
+
+}
---
+++ src/Facades/Html.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ferranfg\Laravextra\Form;
+
+use Illuminate\Html\HtmlFacade;
+
+class Html extends HtmlFacade
+{
+
+}
