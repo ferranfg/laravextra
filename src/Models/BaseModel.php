cd773cdb09454ced054c6b3938bbe6eeba151ad0
Ferran Figueredo <ferranfg@gmail.com>
2016-01-07 12:12:56 +0100

    Initial commit

---
+++ src/Models/BaseModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ferranfg\Laravextra\Models;
+
+use Form;
+use Input;
+
+trait BaseModel
+{
+	public function input($prop, $name = null, $type = 'text', $extra = [])
+	{
+		if (is_null($name)) $name = $prop;
+
+		$extra = array_merge(['class' => 'form-control', 'id' => $prop], $extra);
+
+		return Form::$type($name, Input::old($name, $this->$prop), $extra);
+	}
+
+	public function datetime($prop, $name = null)
+	{
+		return $this->input($prop, $name, 'text', ['class' => 'form-control datetime']);
+	}
+
+	public function textarea($prop, $name = null)
+	{
+		return $this->input($prop, $name, 'textarea', ['class' => 'form-control', 'rows' => 4]);
+	}
+}
